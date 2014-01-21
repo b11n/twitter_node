ec6dac8d9fe00f051b6abfe4e93ef903f4f0cc8d
kingbalan <balan1.618@gmail.com>
2014-01-22 00:59:04 +0530

    added sentiment analysis

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var util = require('util'),
     twitter = require('twitter');
+
+var sentiment = require('sentiment');
+
 var twit = new twitter({
     consumer_key: 'yOwkPRTMVYDiG1sdjc0PA',
     consumer_secret: 'PiiNjFpyC0VoIm2aQPdGas4vqLCg4zsYjEPLABoIs8',
@@ -8,14 +11,14 @@ var twit = new twitter({
 });
 
 
-key = "AAP";
+key = "data";
 
 var io = require('socket.io').listen(8111);
 
 io.sockets.on('connection', function (socket) {
 
 		setInterval(function(){
-			socket.emit('news', { value: count, keyword:key });
+			//socket.emit('news', { value: count, keyword:key });
 			count = 0;
 		},1000)
 
@@ -28,7 +31,13 @@ count = 0;
 
 twit.stream('filter', {track:key}, function(stream) {
     stream.on('data', function(data) {
-        //console.log(data.text)
+        if(data.lang == 'en')
+        {
+            sentiment(data.text, function (err, result) {
+                    console.log(data.text);
+                    console.log(result.score);    // Score: -2, Comparative: -0.666
+            });
+        }
         count++
     });
  
